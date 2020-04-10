deb58ed81e46a805139b06ad5a8eed074e92d2f3
Tero Vuori <tero.vuori@outlook.com>
2020-04-11 01:41:04 +0300

    checkstyle

--- src/main/java/ohtu/KonsoliIO.java
+++ src/main/java/ohtu/KonsoliIO.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ohtu;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Tero
- */
 public class KonsoliIO implements IO{
     private Scanner lukija;
 
@@ -25,6 +17,4 @@ public class KonsoliIO implements IO{
     public void print(String m) {
         System.out.println(m);
     }
-
-    
 }
--- src/main/java/ohtu/Laskin.java
+++ src/main/java/ohtu/Laskin.java
@@ -13,19 +13,21 @@ private IO io;
         while( true ) {
             io.print("luku 1: ");
             int luku1 = io.nextInt();
-            if ( luku1==-9999  ) return;
-
+            if ( luku1 == -9999  ) {
+                return;
+            }
             io.print("luku 2: ");
             int luku2 = io.nextInt();
-            if ( luku2==-9999 ) return;
-
+            if ( luku2 == -9999 ) {
+                return;
+            }
             int vastaus = laskeSumma(luku1, luku2);
-            io.print("summa: "+vastaus+"\n");
+            io.print("summa: " + vastaus + "\n");
         }
     }
-
+    
     private int laskeSumma(int luku1, int luku2) {
-        return luku1+luku2;
+        return luku1 + luku2;
     }
 
 }
